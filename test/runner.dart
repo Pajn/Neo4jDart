efd52929d139119586647bf53a6279fff2a6d7fb
Rasmus Eneman <rasmus@eneman.eu>
2015-03-15 00:52:40 +0100

    test: Add repository tests to runner

--- test/runner.dart
+++ test/runner.dart
@@ -1,7 +1,9 @@
 import 'specs/cypher.dart' as cypher;
+import 'specs/repository.dart' as repository;
 import 'specs/transactions.dart' as transactions;
 
 main() {
   cypher.main();
+  repository.main();
   transactions.main();
 }
