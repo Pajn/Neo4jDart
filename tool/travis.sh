b126c6d2d3559fb024238f1a3a07a549d2f165c4
Rasmus Eneman <rasmus@eneman.eu>
2015-03-18 15:36:12 +0100

    chore(Travis): Only run coveralls on dart dev builds as 1.8 are rune without --enable-async

--- tool/travis.sh
+++ tool/travis.sh
@@ -21,16 +21,16 @@ if [ "$TRAVIS_DART_VERSION" == "stable" ]; then
     dart --checked --enable-async test/runner.dart
 else
     dart --checked test/runner.dart
-fi
 
-# If the COVERALLS_TOKEN token is set on travis
-# Install dart_coveralls
-# Rerun tests with coverage and send to coveralls
-if [ "$COVERALLS_TOKEN" ]; then
-  pub global activate dart_coveralls
-  pub global run dart_coveralls report \
-    --token $COVERALLS_TOKEN \
-    --retry 2 \
-    --exclude-test-files \
-    test/runner.dart
+    # If the COVERALLS_TOKEN token is set on travis
+    # Install dart_coveralls
+    # Rerun tests with coverage and send to coveralls
+    if [ "$COVERALLS_TOKEN" ]; then
+      pub global activate dart_coveralls
+      pub global run dart_coveralls report \
+        --token $COVERALLS_TOKEN \
+        --retry 2 \
+        --exclude-test-files \
+        test/runner.dart
+    fi
 fi
