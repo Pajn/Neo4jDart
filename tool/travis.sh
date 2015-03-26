0437ca7e2dcb888be90b070c22c582ac95d43161
Rasmus Eneman <rasmus@eneman.eu>
2015-03-26 11:38:34 +0100

    chore(Travis): Fix stable builds in travis

--- tool/travis.sh
+++ tool/travis.sh
@@ -17,20 +17,16 @@ function stopDatabase {
 trap stopDatabase EXIT
 
 # Run the tests.
-if [ "$TRAVIS_DART_VERSION" == "stable" ]; then
-    dart --checked --enable-async test/runner.dart
-else
-    dart --checked test/runner.dart
+dart --checked test/runner.dart
 
-    # If the COVERALLS_TOKEN token is set on travis
-    # Install dart_coveralls
-    # Rerun tests with coverage and send to coveralls
-    if [ "$COVERALLS_TOKEN" ]; then
-      pub global activate dart_coveralls
-      pub global run dart_coveralls report \
-        --token $COVERALLS_TOKEN \
-        --retry 2 \
-        --exclude-test-files \
-        test/runner.dart
-    fi
+# If the COVERALLS_TOKEN token is set on travis
+# Install dart_coveralls
+# Rerun tests with coverage and send to coveralls
+if [ "$COVERALLS_TOKEN" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token $COVERALLS_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    test/runner.dart
 fi
