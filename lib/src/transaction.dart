488b1b9ab3f6bdb78a889e2785f59054f101c645
Rasmus Eneman <rasmus@eneman.eu>
2015-04-01 16:37:38 +0200

    Revert "Revert "perf: Use streaming API""
    This reverts commit 30a793ec89666d3c74ed216fbe47bf5f94503998.

--- lib/src/transaction.dart
+++ lib/src/transaction.dart
@@ -31,6 +31,7 @@ class Transaction {
     var response = await http.post(url, headers: {
         'Accept': 'application/json; charset=UTF-8',
         'Content-Type': 'application/json; charset=UTF-8',
+        'X-Stream': 'true',
       },
       body: body
     );
